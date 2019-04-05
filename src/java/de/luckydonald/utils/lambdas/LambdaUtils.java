92ba34101c5a2c0c2f925829bfa467d3e64fb3c5
luckydonald <mac2019.git@luckydonald.de>
2019-04-05 18:03:04 +0200

    Stop incorrect/missing javadocs from breaking the build. https://blog.joda.org/2014/02/turning-off-doclint-in-jdk-8-javadoc.html

--- src/java/de/luckydonald/utils/lambdas/LambdaUtils.java
+++ src/java/de/luckydonald/utils/lambdas/LambdaUtils.java
@@ -4,16 +4,18 @@ import java.util.concurrent.Callable;
 
 public class LambdaUtils {
     /**
-     * Use methods thowing errors.
+     * Use methods throwing errors.
      *
-     * <br /><br />
-     * <b>Example:</b><br />
+     * <br><br>
+     * <b>Example:</b><br>
      * We have a stream of <code>obj</code> elements, and wanna call <code>obj.someCall()</code> basically,
      * but we don't want to keep them elements if that method thrown an error.
-     * <pre>return stream.filter(obj -> uncheckCall(obj::someCall))</pre>
-     * @param callable
-     * @param <T>
-     * @return
+     * <pre>return stream.filter(obj -&gt; uncheckCall(obj::someCall))</pre>
+     *
+     * @param callable  A lambda to execute.
+     * @param <T>  The return type of the lambda.
+     * @return  Returns what the lambda returns.
+     *
      * @link https://stackoverflow.com/a/19757456/3423324#java-8-lambda-streams-filter-by-method-with-exception
      */
     public static <T> T uncheckCall(Callable<T> callable) {
